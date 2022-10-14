6cd62e3a6c63b8dd55dfd02a814b1bae2339d000
Gofurovlazizbek2002 <lazizbekgofurov997@gmail.com>
2022-10-14 06:53:32 +0500

    build_func02.py

--- build_func02.py
+++ build_func02.py
@@ -1,2 +1,6 @@
 # the task condition is placed in the README file
+number=3*(7/5-9/4)
+answer=round(number, 2)
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
+
+print(answer)
